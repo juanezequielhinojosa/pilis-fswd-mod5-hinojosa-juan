9cd813f291dd51b0c9d31ac066baad1f6d6601f3
Juan Hinojosa <juanezequielhinojosa@gmail.com>
2023-04-24 11:14:58 -0300

    creando componente de busqueda

---
+++ src/components/search-bar/SearchBar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { TextInput, View } from 'react-native'
+import { styles } from './SearchBar.styles'
+import { Ionicons } from '@expo/vector-icons'
+import { COLORES } from '../../utils/theme'
+
+export const SearchBar = ({ handleSearch, searchQuery }) => {
+  return (
+    <View style={styles.searchContainer}>
+      <Ionicons name='search' size={30} color={COLORES.primary} />
+      <TextInput
+        placeholder='¿Cual Evento Politico Buscas?'
+        style={styles.buscador}
+        onChangeText={handleSearch}
+        value={searchQuery}
+      />
+    </View>
+  )
+}
