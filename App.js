aad8497ac7d26c056712dd57654f9e3617440b77
Juan Hinojosa <juanezequielhinojosa@gmail.com>
2023-04-24 12:56:22 -0300

    reorganizando el archivo app.js

--- App.js
+++ App.js
@@ -1,20 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { NavigationContainer } from "@react-navigation/native";
+import { MainStackScreen } from "./src/screens/event-list/MainStackScreen";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { EventDetailScreen } from "./src/screens/event-detail/EventDetailScreen";
+import { EventDetailWebScreen } from "./src/screens/event-detail/EventDetailWebScreen";
+import { UserProvider } from "./src/contexts/UserContext";
+
+const EventsListStack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!!</Text>
+    <>
+      <UserProvider>
+        <NavigationContainer>
+          <EventsListStack.Navigator screenOptions={{ headerShown: false }}>
+            {/* //Pantallas con Tab */}
+            <EventsListStack.Screen name="Main" component={MainStackScreen} />
+            {/* //Pantallas sin Tab */}
+            <EventsListStack.Screen
+              name="EventDetail"
+              component={EventDetailScreen}
+            />
+            <EventsListStack.Screen
+              name="EventDetailWeb"
+              component={EventDetailWebScreen}
+            />
+          </EventsListStack.Navigator>
+        </NavigationContainer>
+      </UserProvider>
+
       <StatusBar style="auto" />
-    </View>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
