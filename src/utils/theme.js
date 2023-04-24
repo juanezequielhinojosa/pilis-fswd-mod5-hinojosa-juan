dbc6b3eb6aff31d4a9393d57c2829acb2b68eb0f
Juan Hinojosa <juanezequielhinojosa@gmail.com>
2023-04-24 12:55:04 -0300

    agregando utilidades

---
+++ src/utils/theme.js
@@ -0,0 +1,27 @@
+export const COLORES = {
+  primary: '#FF585D',
+  secondaty: '#F9777B',
+  text: '#263238',
+  inactive: '#A0A0A0',
+  white: '#FFFFFF',
+  black: '#110e14',
+  grey: '#F1F1F1'
+}
+export const SPACING = {
+  xs: 4,
+  sm: 8,
+  md: 16,
+  lg: 24,
+  xl: 40,
+  xxl: 48,
+  xxxl: 64
+}
+export const FONT_SIZE = {
+  xs: 4,
+  sm: 8,
+  md: 16,
+  lg: 24,
+  xl: 40,
+  xxl: 48,
+  xxxl: 64
+}
