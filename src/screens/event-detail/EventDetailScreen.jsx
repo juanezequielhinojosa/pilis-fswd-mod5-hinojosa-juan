dbb0e523ef67b50fce893b6ffd246877ff1ab736
Juan Hinojosa <juanezequielhinojosa@gmail.com>
2023-04-25 14:41:58 -0300

    agregando mas funcionalidades

--- src/screens/event-detail/EventDetailScreen.jsx
+++ src/screens/event-detail/EventDetailScreen.jsx
@@ -1,14 +1,20 @@
 import React, { useContext } from 'react'
-import { View, ScrollView, Image, Text } from 'react-native'
+import { View, ScrollView, Image, Text, TouchableOpacity } from "react-native";
 import MapView, { Marker } from 'react-native-maps'
 import { styles } from './EventDetailScreen.styles'
 import { AntDesign } from '@expo/vector-icons'
 import { COLORES } from '../../utils/theme'
 import { Link } from '@react-navigation/native'
 import { UserContext } from '../../contexts/UserContext'
+import { useNavigation } from "@react-navigation/native";
 export const EventDetailScreen = ({ route }) => {
   const { item } = route.params
   const { currentUser } = useContext(UserContext)
+  const navigation = useNavigation()
+
+  const handleLogin = () => {
+      navigation.navigate("Profile");
+  }
 
   return (
     <ScrollView style={styles.container}>
@@ -19,7 +25,7 @@ export const EventDetailScreen = ({ route }) => {
               key={idx}
               source={{ uri: `https://drive.google.com/uc?id=${image}` }}
               style={styles.image}
-              resizeMode='cover'
+              resizeMode="cover"
             />
           ))}
         </ScrollView>
@@ -30,14 +36,14 @@ export const EventDetailScreen = ({ route }) => {
         <Text style={styles.names}>{item.names}</Text>
         <Text style={styles.title}>{item.presentacion}</Text>
         <View style={styles.fechaContainer}>
-          <AntDesign name='calendar' size={20} color={COLORES.primary} />
+          <AntDesign name="calendar" size={20} color={COLORES.primary} />
           <Text style={styles.fecha}>{item.fecha}</Text>
         </View>
-        {currentUser && (
+        {currentUser ? (
           <>
             <Link
               style={styles.webButton}
-              to={{ screen: 'EventDetailWeb', params: { url: item.url } }}
+              to={{ screen: "EventDetailWeb", params: { url: item.url } }}
             >
               Visitar
             </Link>
@@ -47,21 +53,25 @@ export const EventDetailScreen = ({ route }) => {
                 latitude: item.locationCoordinates.latitude,
                 longitude: item.locationCoordinates.longitude,
                 latitudeDelta: 0.003,
-                longitudeDelta: 0.003
+                longitudeDelta: 0.003,
               }}
             >
               <Marker
                 coordinate={{
                   latitude: item.locationCoordinates.latitude,
-                  longitude: item.locationCoordinates.longitude
+                  longitude: item.locationCoordinates.longitude,
                 }}
                 title={item.title}
               />
             </MapView>
           </>
+        ) : (
+          <TouchableOpacity style={styles.button} onPress={handleLogin}>
+            <Text style={styles.webButton}>Mas Info</Text>
+          </TouchableOpacity>
         )}
         <Text style={styles.description}>{item.description}</Text>
       </View>
     </ScrollView>
-  )
+  );
 }
